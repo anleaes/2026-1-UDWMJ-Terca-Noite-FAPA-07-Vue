659552ab4e427993f20efc6bc1f5509d8543043f
Vinicius Santos <vinciussantos15@gmail.com>
2026-05-31 15:03:00 -0300

    style: formatação código com Prettier

--- projeto_a3/src/router/routes.js
+++ projeto_a3/src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/LojaPage.vue') }],
   },
-   {
+  {
     path: '/descricoes/adicionar',
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/AddDescricaoPage.vue') }],
